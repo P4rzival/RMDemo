3cc02e68fc05266683f8852196a28fe167e12dcc
P4rzival <thafiz5@gmail.com>
2020-02-23 16:22:07 -0800

    Added Taseen Bio info, tab name still needs to be modified

--- app/src/main/java/com/github/p4rzival/rmdemo/teambio/ui/main/PageViewModel.java
+++ app/src/main/java/com/github/p4rzival/rmdemo/teambio/ui/main/PageViewModel.java
@@ -12,7 +12,7 @@ public class PageViewModel extends ViewModel {
     private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
         @Override
         public String apply(Integer input) {
-            return "Hello world from section: " + input;
+            return "Hey I'm Taseen, a member of the Radius Message dev team, ths is a test application for Sprint 0 so we can all et familiar with the commit work process";
         }
     });
 
