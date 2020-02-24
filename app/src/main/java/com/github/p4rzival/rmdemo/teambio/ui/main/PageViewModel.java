c01bbcb7cf3967fe0bc52f09ebe1ae60887615e9
Hafiz <taseen.hafiz264@SIE-1222-09.local>
2020-02-24 13:16:50 -0800

    Tried Adding my name correctly so that it only shows up on my tab,is still does not do that. I am trying to access the Tab_Titles from the PageViewModel class but it's not working

--- app/src/main/java/com/github/p4rzival/rmdemo/teambio/ui/main/PageViewModel.java
+++ app/src/main/java/com/github/p4rzival/rmdemo/teambio/ui/main/PageViewModel.java
@@ -12,8 +12,16 @@ public class PageViewModel extends ViewModel {
     private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
         @Override
         public String apply(Integer input) {
-            return "Hey I'm Taseen, a member of the Radius Message dev team, ths is a test application for Sprint 0 so we can all et familiar with the commit work process";
-        }
+            String returnVar = "0";
+            SectionsPagerAdapter SPA;
+            for(int i = 0; i < SPA.getCount(); i++) {
+                if(toString(SPA.getPageTitle(i)).contains("Taseen"))
+                {
+                    returnVar = "Hey I'm Taseen, a member of the Radius Message dev team, this is a test application for Sprint 0 so we can all get familiar with the commit work process";
+                }
+            }
+            return returnVar;
+    }
     });
 
     public void setIndex(int index) {
